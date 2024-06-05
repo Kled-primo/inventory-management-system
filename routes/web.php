2de85cf0c8f9b4268b42741c15a58074a4223d12
Junn Eric Timoteo <juntyms@yahoo.com>
2024-06-05 19:48:34 +0400

    fix: producttype to product_type

--- routes/web.php
+++ routes/web.php
@@ -66,7 +66,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('/customers', CustomerController::class);
     Route::resource('/suppliers', SupplierController::class);
     Route::resource('/categories', CategoryController::class);
-    //Route::resource('/producttype', ProductType::class);
+    Route::resource('/producttype', ProductType::class);
     Route::resource('/units', UnitController::class);
 
     //Route Product Forecast
